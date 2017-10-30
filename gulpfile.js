5febb2c27d06f207db211cca4fd9b0bd1b28b203
Alejandra Beltramen <alejandra.beltramen@gmail.com>
2017-10-30 12:15:10 -0300

    Update port number on gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -104,7 +104,7 @@ gulp.task('clean:tmp', function (cb) {
 });
 
 gulp.task('start:client', ['start:server', 'styles'], function () {
-  openURL('http://localhost:9000');
+  openURL('http://localhost:4000');
 });
 
 gulp.task('start:server', function() {
